18cfbed06704eb080f7a9bb7f3a59e1a1000e909
kala <lissettecarlr@163.com>
2021-07-06 14:45:14 +0800

    fix Port

--- port/port_scan.py
+++ port/port_scan.py
@@ -1,20 +1,36 @@
+# from scapy.all import *
+ 
+# SYN=IP(dst="192.168.2.217")/TCP(dport=22,flags='S')
+ 
+# print ("-- Send SYN --")
+# #SYN.display()
+ 
+# print("\n\n-- SYN Received --")
+# response = sr1(SYN,timeout=0.5,verbose=0)
+# # response.display()
+# print(response)
+ 
+# if int(response[TCP].flags) == 18:
+#     print("\n\n-- Sent ACK --")
+#     A = IP(dst="192.168.2.244")/TCP(dport=21,flags='A',ack=(response[TCP].seq+1))
+#     A.display()
+#     print("\n\n-- ACK Received --")
+#     response2 = sr1(A,timeout=1,verbose=0)
+#  #   response2.display()
+# else:
+#     print("SYN/ACK not returned")
+
 from scapy.all import *
- 
-SYN=IP(dst="192.168.2.244")/TCP(dport=21,flags='S')
- 
-print ("-- Send SYN --")
-#SYN.display()
- 
-print("\n\n-- SYN Received --")
-response = sr1(SYN,timeout=1,verbose=0)
-#response.display()
- 
-if int(response[TCP].flags) == 18:
-    print("\n\n-- Sent ACK --")
-    A = IP(dst="192.168.2.244")/TCP(dport=21,flags='A',ack=(response[TCP].seq+1))
-    A.display()
-    print("\n\n-- ACK Received --")
-    response2 = sr1(A,timeout=1,verbose=0)
- #   response2.display()
-else:
-    print("SYN/ACK not returned")
+def port_scan(port):
+    answer= sr1(IP(dst="72.14.207.99") / (TCP(dport=int(port), flags="S")),timeout=0.5,verbose=0)
+    if answer == None:
+        # print(port,"not ack")
+        pass
+    else:
+        if answer[TCP].flags == 18:
+            print(port,"is Open")
+        if answer[TCP].flags == 20:
+            print(port,"is Closed")
+
+for i in range(81):        
+    port_scan(i)
